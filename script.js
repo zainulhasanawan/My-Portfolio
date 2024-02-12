834f97d9475e0b2efcb61e01d56e8f5206bbfb72
zainulhasanawan <zainulhassanawan001@gmail.com>
2024-02-12 23:32:06 +0500

    Design Improvements & Updated Content

--- script.js
+++ script.js
@@ -1,6 +1,63 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.getElementById("burger");
+  const navUl = document.getElementById("navUl");
+  const overlay = document.getElementById("overlay");
+
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    navUl.classList.toggle("show");
+    overlay.classList.toggle("active");
+  });
+
+  overlay.addEventListener("click", function () {
+    burger.classList.remove("active");
+    navUl.classList.remove("show");
+    overlay.classList.remove("active");
+  });
+
+  const navLinks = document.querySelectorAll(".nav-links");
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      burger.classList.remove("active");
+      navUl.classList.remove("show");
+      overlay.classList.remove("active");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var aboutTextParagraphs = document.querySelectorAll(".about-text");
+
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function handleScroll() {
+    aboutTextParagraphs.forEach(function (paragraph) {
+      if (isElementInViewport(paragraph)) {
+        paragraph.classList.add("fade-in");
+      }
+    });
+
+    if (
+      Array.from(aboutTextParagraphs).every((p) =>
+        p.classList.contains("fade-in")
+      )
+    ) {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }
+
+  window.addEventListener("scroll", handleScroll);
+
+  handleScroll();
+});
